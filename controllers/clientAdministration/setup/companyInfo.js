1f8c4bc3f6ad8c7a6ebfcf863dcc98eda466a73c
Paul Park <camo1018@yahoo.com>
2014-07-17 23:17:51 -0400

    Fixed company info

--- controllers/clientAdministration/setup/companyInfo.js
+++ controllers/clientAdministration/setup/companyInfo.js
@@ -2,6 +2,6 @@ module.exports = function(app, modules) {
     var section = '/clientAdministration/setup/';
 
     app.get(section + 'companyInfo', function(req, res) {
-        res.render('clientAdministration/setup/setupWizard.html');
+        res.render('clientAdministration/setup/companyInfo.html');
     });
 }
