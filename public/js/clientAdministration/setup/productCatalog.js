bdf2ff4a9083bcdc40f999255a004e449ab0a6ca
Paul Park <camo1018@yahoo.com>
2014-07-29 17:40:28 -0400

    Bulk of the UI work done on Product Catalog

--- public/js/clientAdministration/setup/productCatalog.js
+++ public/js/clientAdministration/setup/productCatalog.js
@@ -2,3 +2,10 @@
  * Created by Paul on 7/17/2014.
  */
 ko.applyBindings(productCatalogViewmodel);
+
+$(function() {
+    $('#catalog-detail-product-container').masonry({
+        columnWidth: 200,
+        itemSelector: '.catalog-detail-panel-product'
+    });
+});
--- public/js/viewmodels/productCatalog.js
+++ public/js/viewmodels/productCatalog.js
@@ -1,4 +1,16 @@
 function ProductCatalogViewmodel() {
+
+    this.products = [
+        { name: 'Test 1', value: '5500', imgUrl: '/img/image.png' },
+        { name: 'Test 2', value: '4000', imgUrl: '/img/image.png' },
+        { name: 'Test 3', value: '2555', imgUrl: '/img/image.png' },
+        { name: 'Test 4', value: '1100', imgUrl: '/img/image.png' },
+        { name: 'Test 5', value: '2555', imgUrl: '/img/image.png' },
+        { name: 'Test 6', value: '2576', imgUrl: '/img/image.png' },
+        { name: 'Test 7', value: '5129', imgUrl: '/img/image.png' },
+        { name: 'Test 8', value: '1000', imgUrl: '/img/image.png' }
+    ];
+
     this.stepNumber = ko.observable(2);
 }
 var productCatalogViewmodel = new ProductCatalogViewmodel();
