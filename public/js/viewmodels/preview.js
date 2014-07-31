a9d544d9ec68a2654964e5cd89cc68b70c22fc8c
Paul Park <camo1018@yahoo.com>
2014-07-30 23:45:48 -0400

    Finished UI work for Preview

--- public/js/viewmodels/preview.js
+++ public/js/viewmodels/preview.js
@@ -1,4 +1,15 @@
 function PreviewViewmodel() {
+    this.selectedProducts = [
+        { name: 'Test 1', price: '5500', imgUrl: '/img/image.png' },
+        { name: 'Test 2', price: '4000', imgUrl: '/img/image.png' },
+        { name: 'Test 3', price: '2555', imgUrl: '/img/image.png' },
+        { name: 'Test 4', price: '1100', imgUrl: '/img/image.png' },
+        { name: 'Test 5', price: '2555', imgUrl: '/img/image.png' },
+        { name: 'Test 6', price: '2576', imgUrl: '/img/image.png' },
+        { name: 'Test 7', price: '5129', imgUrl: '/img/image.png' },
+        { name: 'Test 8', price: '1000', imgUrl: '/img/image.png' }
+    ];
+
     this.stepNumber = ko.observable(3);
 }
 var previewViewmodel = new PreviewViewmodel();
