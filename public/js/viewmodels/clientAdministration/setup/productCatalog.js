bfb7863d1208f6ff09a625d28a393d439b208ce9
Paul Park <camo1018@yahoo.com>
2014-09-03 19:43:49 -0400

    Refactored getting of products

--- public/js/viewmodels/clientAdministration/setup/productCatalog.js
+++ public/js/viewmodels/clientAdministration/setup/productCatalog.js
@@ -13,20 +13,21 @@ function ProductCatalogViewmodel(categories, products, resellers) {
     self.setCategory = function(index) {
         self.currentCategoryIndex(index);
         var params = { type: categories[self.currentCategoryIndex()].stepHeaderName, reseller: self.currentReseller().id };
-        $.get('/clientAdministration/setup/getProducts', params, function(products) {
-            self.products(products);
-            self.refreshMasonry();
-        });
+        self.getProducts(params);
     };
 
     self.setReseller = function(index) {
         self.currentReseller(self.resellers()[index]);
         var params = { type: categories[self.currentCategoryIndex()].stepHeaderName, reseller: self.currentReseller().id };
+        self.getProducts(params);
+    };
+
+    self.getProducts = function(params) {
         $.get('/clientAdministration/setup/getProducts', params, function(products) {
             self.products(products);
             self.refreshMasonry();
-        })
-    };
+        });
+    }
 
     self.refreshMasonry = function() {
         if (typeof masonry !== 'undefined') {
